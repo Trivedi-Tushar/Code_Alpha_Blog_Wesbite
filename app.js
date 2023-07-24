cf248e610f55a422fcfc2352e89676e8ef9ea5fc
Trivedi-Tushar <122623241+Trivedi-Tushar@users.noreply.github.com>
2023-07-24 07:00:53 +0530

    Added all new files

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Welcome to our full-responsive blogging platform! Whether you're a seasoned writer or just starting your blogging journey, our user-friendly interface allows you to sign in and effortlessly create your own blog. Share your thoughts, experiences, and expertise with the world, and explore a range of customization options to make your blog uniquely yours. Join our community of passionate bloggers today and embark on an exciting writing adventure!";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
